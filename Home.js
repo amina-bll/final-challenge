9c7dc4bf44f94ef4f1f840e957c0c0f93f314dd2
amina-bll <belal.amina14@gmail.com>
2025-05-01 12:05:43 +0100

    update

---
+++ Home.js
@@ -0,0 +1,11 @@
+document.querySelector('.button').addEventListener('click', function () {
+    window.location.href = './dashbord.html'; 
+});
+
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navLinks = document.querySelector(".nav-links");
+    const overlay = document.querySelector(".overlay");
+  
+  })
